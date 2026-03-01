3c7baf03786eb2ff19e9d9cd3398b8d9f5dc6cf6
Thiagohonorio1 <honoriot794@gmail.com>
2026-03-01 02:59:42 +0000

    nivel novato

--- supertrunfo.c
+++ supertrunfo.c
@@ -1,1 +1,48 @@
+#include <stdio.h>
 
+int main (){
+
+    int carta;
+    int estado;
+    char codigo [50];
+    char cidade [50];
+    int populaçao;
+    float area;
+    float pib;
+    int pontos_turisticos;
+
+    //entrada de dados 
+
+    printf ("estado: SP \n");
+    scanf ("%d", &estado);
+
+    printf ("codigo: A01 \n");
+    scanf ("%s", codigo);
+
+    printf ("nome da cidade: sao paulo \n");
+    scanf ("%s", cidade);
+
+    printf ("populaçao: 12325000 \n");
+    scanf ("%d", &populaçao);
+
+    printf ("area: 1521.11 km² \n");
+    scanf ("%f", &area);
+
+    printf ("pib 699.28 bilhoes de reais \n");
+    scanf ("%f", &pib);
+
+    printf ("pontos turisticos \n");
+    scanf ("%d", &pontos_turisticos);
+
+    //saida de dados
+
+    printf ("carta 1 \n");
+    printf ("")
+
+
+
+
+
+
+
+}
